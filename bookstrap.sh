510be0b0cb3e68b832c4d5921067c2ae22019510
Chris Continanza <christopher.continanza@gmail.com>
2010-11-02 20:33:52 -0600

    not needed

--- bookstrap.sh
+++
@@ -1,5 +0,0 @@
-rails rails_cerberus 
-cd rails_cerberus
-script/rails generate scaffold Card user:string nick:string after_hours:string expires:date valid_from:date disabled:string magic:string
-script/rails generate scaffold Door name:string default_unlocked:string
-script/rails generate scaffold AccessLog logged:datetime door:string action:string detail:string
