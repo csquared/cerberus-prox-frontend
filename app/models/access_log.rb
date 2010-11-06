122926d8f2a62ab163ca5ea4b3238af0ef46ee1e
Chris Continanza <christopher.continanza@gmail.com>
2010-11-05 20:52:49 -0600

    wrote scope for last denied card

--- app/models/access_log.rb
+++ app/models/access_log.rb
@@ -1,3 +1,9 @@
 class AccessLog < ActiveRecord::Base
   set_table_name :access_log
+
+  scope :all_last_denied, :conditions => { :action => "DENY" }, :order => "logged DESC", :limit => 1
+  
+  def last_denied
+    all_last_denied.first
+  end
 end
