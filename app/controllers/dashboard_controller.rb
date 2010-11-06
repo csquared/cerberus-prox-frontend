3ff55b0ea31cc77db63a369f39ac55593ac7354b
Chris Continanza <christopher.continanza@gmail.com>
2010-11-05 23:28:19 -0600

    fix num of cards on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
   
   # let's show some stats!
   def index
-    @total_enabled_users = Card.where(:disabled => "N").count
+    @total_enabled_users = Card.where(:disabled => ["N",'']).count
   end
 end
